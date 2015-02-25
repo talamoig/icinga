679df51b3b0ffb7533341267b29b5ad1381e2544
desalvo <Alessandro.DeSalvo@roma1.infn.it>
2015-02-25 16:41:28 +0100

    added support for PostgreSQL

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -1,21 +1,9 @@
 class icinga::configure {
 
-  define icinga_feature {
-    file{$title:
-      ensure => link,
-      path   => "${icinga::params::features_enabled_path}/${title}.conf",
-      target => "${icinga::params::features_avail_path}/${title}.conf",
-      notify => Class['icinga::service']
-    }    
-  }
+  icinga::icinga_feature{ $icinga::params::features_all: }
 
-  icinga_feature{ $icinga::params::features_all: }
-
-  if $icinga::params::dbtype=='mysql' {
-    file{"${icinga::params::features_avail_path}/ido-mysql.conf" :
-      content => template('icinga/ido-mysql.erb'),
-      notify  => Class['icinga::service'],
-    }
+  file{"${icinga::params::features_avail_path}/ido-${::icinga::params::dbtype}.conf" :
+    content => template("icinga/ido-${::icinga::params::dbtype}.erb"),
+    notify  => Class['icinga::service'],
   }
-  
 }
