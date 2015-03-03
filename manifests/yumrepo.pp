e8645647893c340b2b3b410a99712b95b0dcd92c
talamoig <ivano.talamo@gmail.com>
2015-03-03 07:15:55 +0100

    updated some parameters for compatibility with puppet 3.1

--- manifests/yumrepo.pp
+++ manifests/yumrepo.pp
@@ -2,9 +2,8 @@ class icinga::yumrepo {
 
   if $icinga::with_repo {
     yumrepo {'icinga-stable-release':
-      ensure => present,
       baseurl => 'http://packages.icinga.org/epel/$releasever/release/',
-      enabled => true,
+      enabled => '1',
       gpgcheck => '1',
       gpgkey => 'http://packages.icinga.org/icinga.key',
     }
